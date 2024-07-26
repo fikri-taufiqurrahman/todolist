58d61b8364f45d76be13c96d7928350014c15dcf
fikri-taufiqurrahman <fikritaufiqurrahman7@gmail.com>
2024-07-26 20:37:59 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/login";
 import Register from "./components/register";
 import Tasks from "./components/tasks";
