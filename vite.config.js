35482fb8ef578841bb7ddb34a70a377b3abaf98a
fikri-taufiqurrahman <fikritaufiqurrahman7@gmail.com>
2024-07-26 11:19:59 +0700

    Pesan commit Anda

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/todolist/"
+    base: "/todolist/",
+    build: {
+        outDir: 'dist'
+    }
 })
